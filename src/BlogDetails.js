35bc785f5f4516b0d131e791950593a675b878c4
Savinu Anusara <savinu0002@gmail.com>
2023-12-20 20:44:32 +0530

    The completed comit with all feachers with the local host

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+
+    }
 
     return (  
         <div className="blog-details">
@@ -16,7 +26,9 @@ const BlogDetails = () => {
                     <h2> {blog.title} </h2>
                     <p> Written by {blog.author} </p>
                     <div> {blog.body} </div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
+            
             )}
 
 
